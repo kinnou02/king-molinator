e98f8e10ab485dd5955e8392bc85c92a05308a36
kekoas <kekoas@61996b99-9865-41db-908e-c3a2ce7c955a>
2012-05-09 04:17:30 +0000

    Release
    git-svn-id: https://repos.curseforge.com/rift/king-molinator/trunk@377 61996b99-9865-41db-908e-c3a2ce7c955a

--- Masters/Darkening_Deeps/Braxtepel.lua
+++ Masters/Darkening_Deeps/Braxtepel.lua
@@ -195,11 +195,11 @@ function MOD:Death(UnitID)
 	return false
 end
 
-function MOD:UnitHPCheck(unitDetails, unitID)	
-	if unitDetails and unitID then
-		if not unitDetails.player then
-			if self.Bosses[unitDetails.name] then
-				local BossObj = self.Bosses[unitDetails.name]
+function MOD:UnitHPCheck(uDetails, unitID)	
+	if uDetails and unitID then
+		if not uDetails.player then
+			if self.Bosses[uDetails.name] then
+				local BossObj = self.Bosses[uDetails.name]
 				if not self.EncounterRunning then
 					self.EncounterRunning = true
 					self.StartTime = Inspect.Time.Real()
