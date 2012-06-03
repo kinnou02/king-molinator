9b3debf89cdff6f4fe71164b5983e6566498c8d8
kekoas <kekoas@61996b99-9865-41db-908e-c3a2ce7c955a>
2012-06-03 03:19:35 +0000

    Translation Fix
    git-svn-id: https://repos.curseforge.com/rift/king-molinator/trunk@393 61996b99-9865-41db-908e-c3a2ce7c955a

--- KingMolinator.lua
+++ KingMolinator.lua
@@ -12,7 +12,7 @@ local LocaleManager = Inspect.Addon.Detail("KBMLocaleManager")
 local KBMLM = LocaleManager.data
 KBMLM.Start(KBM)
 KBM.BossMod = {}
-KBM.Alpha = ".r392"
+KBM.Alpha = ".r393"
 KBM.Event = {
 	Mark = {},
 	System = {
