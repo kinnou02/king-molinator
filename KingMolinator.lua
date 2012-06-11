afae85e5f1394158bcae82d672c003916630b3cb
kekoas <kekoas@61996b99-9865-41db-908e-c3a2ce7c955a>
2012-06-11 17:48:01 +0000

    Resync correct alpha tags
    git-svn-id: https://repos.curseforge.com/rift/king-molinator/trunk@407 61996b99-9865-41db-908e-c3a2ce7c955a

--- KingMolinator.lua
+++ KingMolinator.lua
@@ -12,7 +12,7 @@ local LocaleManager = Inspect.Addon.Detail("KBMLocaleManager")
 local KBMLM = LocaleManager.data
 KBMLM.Start(KBM)
 KBM.BossMod = {}
-KBM.Alpha = ".r405"
+KBM.Alpha = ".r407"
 KBM.Event = {
 	Mark = {},
 	System = {
