27d321d05b22e03db188bbc337bafd213e2e7106
kekoas <kekoas@61996b99-9865-41db-908e-c3a2ce7c955a>
2012-06-13 10:01:47 +0000

    Correct Alpha version listing. Le sigh.
    git-svn-id: https://repos.curseforge.com/rift/king-molinator/trunk@415 61996b99-9865-41db-908e-c3a2ce7c955a

--- KingMolinator.lua
+++ KingMolinator.lua
@@ -12,7 +12,7 @@ local LocaleManager = Inspect.Addon.Detail("KBMLocaleManager")
 local KBMLM = LocaleManager.data
 KBMLM.Start(KBM)
 KBM.BossMod = {}
-KBM.Alpha = ".r413"
+KBM.Alpha = ".r415"
 KBM.Event = {
 	Mark = {},
 	System = {
