b2cc9113facb35c2f4bb9b721fb0d2207347dc61
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2017-05-24 11:46:43 +0200

    add gyel forteress

---
+++ Experts/Prophecy of Ahnket/Gyel_Fortress/GF_Header.lua
@@ -0,0 +1,53 @@
+-- Gyel Fortress Header for King Boss Mods
+-- Written by Maatang
+-- July 2015
+
+KBMPOAIGF_Settings = nil
+chKBMPOAIGF_Settings = nil
+
+local MOD = {
+	Directory = "Experts/Prophecy of Ahnket/Gyel_Fortress/",
+	File = "GF_Header.lua",
+	Header = nil,
+	Enabled = true,
+	IsInstance = true,
+	Name = "Intrepid Gyel Fortress",
+	Type = "Expert",
+	ID = "Intrepid_Gyel_Fortress",
+	Object = "MOD",
+	Rift = "Prophecy of Ahnket",
+}
+
+-- Link Mods
+local AddonData = Inspect.Addon.Detail("KingMolinator")
+local KBM = AddonData.data
+if not KBM.BossMod then
+	return
+end
+
+KBM.RegisterMod(MOD.ID, MOD)
+
+-- Header Dictionary
+MOD.Lang = {}
+MOD.Lang.Main = {}
+MOD.Lang.Main.Name = KBM.Language:Add(MOD.Name)
+MOD.Lang.Main.Name:SetFrench("Forteresse Gyel")
+
+
+MOD.Name = MOD.Lang.Main.Name[KBM.Lang]
+MOD.Descript = MOD.Name
+
+function MOD:AddBosses(KBM_Boss)
+end
+
+function MOD:InitVars()
+end
+
+function MOD:LoadVars()
+end
+
+function MOD:SaveVars()
+end
+
+function MOD:Start()
+end
