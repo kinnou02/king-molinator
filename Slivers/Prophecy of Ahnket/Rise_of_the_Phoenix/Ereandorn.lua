543f1f369e36f8ab5c2e2cc1528ad903a3130a71
Alexandre Jacquin <kinou@rilynett.com>
2017-06-04 18:31:39 +0200

    add Intrepid Rise of the phoenix

---
+++ Slivers/Prophecy of Ahnket/Rise_of_the_Phoenix/Ereandorn.lua
@@ -0,0 +1,270 @@
+﻿-- Ereandorn Boss Mod for King Boss Mods
+-- Written by Paul Snart
+-- Copyright 2011
+--
+
+KBMIROTPEN_Settings = nil
+-- Link Mods
+local AddonData = Inspect.Addon.Detail("KingMolinator")
+local KBM = AddonData.data
+if not KBM.BossMod then
+	return
+end
+local ROTP = KBM.BossMod["Intrepid Rise of the Phoenix"]
+
+local EN = {
+	Directory = ROTP.Directory,
+	File = "Ereandorn.lua",
+	Enabled = true,
+	Instance = ROTP.Name,
+	InstanceObj = ROTP,
+	HasPhases = false,
+	Lang = {},
+	TankSwap = false,
+	ID = "Intrepid Ereandorn",
+	Enrage = 60 * 7,
+	Menu = {},
+	Object = "EN",
+}
+
+EN.Ereandorn = {
+	Mod = EN,
+	Level = 72,
+	Active = false,
+	Name = "Ereandorn",
+	Menu = {},
+	Castbar = nil,
+	CastFilters = {},
+	HasCastFilters = false,
+	AlertsRef = {},
+	MechRef = {},
+	Dead = false,
+	Available = false,
+	UTID = "U1B3DA8B866FF9DAA",
+	UnitID = nil,
+	TimeOut = 5,
+	Triggers = {},
+	Settings = {
+		CastBar = KBM.Defaults.Castbar(),
+		AlertsRef = {
+			Enabled = true,
+			Combustion = KBM.Defaults.AlertObj.Create("red"),
+			Growth = KBM.Defaults.AlertObj.Create("red"),
+			Eruption = KBM.Defaults.AlertObj.Create("orange"),
+		},
+		MechRef = {
+			Enabled = true,
+			Combustion = KBM.Defaults.MechObj.Create("red"),
+		},
+	},
+}
+
+KBM.RegisterMod(EN.ID, EN)
+
+-- Main Unit Dictionary
+EN.Lang.Unit = {}
+EN.Lang.Unit.Ereandorn = KBM.Language:Add(EN.Ereandorn.Name)
+EN.Lang.Unit.Ereandorn:SetGerman()
+EN.Lang.Unit.Ereandorn:SetFrench()
+EN.Lang.Unit.Ereandorn:SetRussian("Эреандорн")
+EN.Lang.Unit.Ereandorn:SetKorean("에레안도른")
+EN.Ereandorn.Name = EN.Lang.Unit.Ereandorn[KBM.Lang]
+EN.Descript = EN.Ereandorn.Name
+
+-- Notify Dictionary
+EN.Lang.Notify = {}
+EN.Lang.Notify.Combustion = KBM.Language:Add('Ereandorn says, "(%a*), how does it feel to burn?"')
+EN.Lang.Notify.Combustion:SetGerman('Ereandorn sagt: "(%a*), wie fühlt es sich an, zu verbrennen?"')
+EN.Lang.Notify.Combustion:SetFrench('Ereandorn dit : "Cà vous fait quel effet de brûler, (%a*) ?"')
+EN.Lang.Notify.Combustion:SetRussian('Эреандорн спрашивает: "Ну как, (%a*), нравится тебе гореть?"')
+EN.Lang.Notify.Combustion:SetKorean('에레안도른이 "(%a*), 타오르는 느낌이 어떠냐?"라고 말합니다.')
+EN.Lang.Notify.Growth = KBM.Language:Add("The corpse of (%a*) will fuel our conquest")
+EN.Lang.Notify.Growth:SetGerman("Der Leichnam von (%a*) wird unsere Eroberung vorantreiben")
+EN.Lang.Notify.Growth:SetFrench("Le cadavre de (%a*) alimentera notre conquête !")
+EN.Lang.Notify.Growth:SetRussian("(%a*) - вот чей труп поможет нам в нашем вторжении!")
+EN.Lang.Notify.Growth:SetKorean("(%a*)의 시체는 우리 승리의 토대가 될 것이다!")
+EN.Lang.Notify.Eruption = KBM.Language:Add("I will rebuild this world in flames!")
+EN.Lang.Notify.Eruption:SetGerman("Ich werde diese Welt in Flammen neu formen!")
+EN.Lang.Notify.Eruption:SetFrench("Je rebâtirai ce monde dans les flammes !")
+EN.Lang.Notify.Eruption:SetRussian("Я предам этот мир огню!")
+EN.Lang.Notify.Eruption:SetKorean("이 세계를 불꽃으로 재건할 것이다!")
+-- Ability Dictionary
+EN.Lang.Ability = {}
+EN.Lang.Ability.Combustion = KBM.Language:Add("Excitable Combustion")
+EN.Lang.Ability.Combustion:SetGerman("Anregbare Verbrennung")
+EN.Lang.Ability.Combustion:SetRussian("Опасное сгорание")
+EN.Lang.Ability.Combustion:SetFrench("Combustion tendue")
+EN.Lang.Ability.Combustion:SetKorean("촉발성 연소")
+EN.Lang.Ability.Growth = KBM.Language:Add("Molten Growth")
+EN.Lang.Ability.Growth:SetGerman("Geschmolzener Wuchs")
+EN.Lang.Ability.Growth:SetRussian("Извержение лавы")
+EN.Lang.Ability.Growth:SetFrench("Éruption liquide")
+EN.Lang.Ability.Growth:SetKorean("고온용암 성장")
+EN.Lang.Ability.Eruption = KBM.Language:Add("Volcanic Eruption")
+EN.Lang.Ability.Eruption:SetGerman("Vulkanausbruch")
+EN.Lang.Ability.Eruption:SetRussian("Извержение вулкана")
+EN.Lang.Ability.Eruption:SetFrench("Éruption volcanique")
+EN.Lang.Ability.Eruption:SetKorean("화산 폭발")
+
+function EN:AddBosses(KBM_Boss)
+	self.MenuName = self.Descript
+	self.Bosses = {
+		[self.Ereandorn.Name] = self.Ereandorn,
+	}
+end
+
+function EN:InitVars()
+	self.Settings = {
+		Enabled = true,
+		CastBar = self.Ereandorn.Settings.CastBar,
+		AlertsRef = self.Ereandorn.Settings.AlertsRef,
+		MechRef = self.Ereandorn.Settings.MechRef,
+		PhaseMon = KBM.Defaults.PhaseMon(),
+		EncTimer = KBM.Defaults.EncTimer(),
+		Alerts = KBM.Defaults.Alerts(),
+		MechSpy = KBM.Defaults.MechSpy(),
+	}
+	KBMIROTPEN_Settings = self.Settings
+	chKBMIROTPEN_Settings = self.Settings
+	
+end
+
+function EN:SwapSettings(bool)
+	if bool then
+		KBMIROTPEN_Settings = self.Settings
+		self.Settings = chKBMIROTPEN_Settings
+	else
+		chKBMIROTPEN_Settings = self.Settings
+		self.Settings = KBMIROTPEN_Settings
+	end
+end
+
+function EN:LoadVars()
+	if KBM.Options.Character then
+		KBM.LoadTable(chKBMIROTPEN_Settings, self.Settings)
+	else
+		KBM.LoadTable(KBMIROTPEN_Settings, self.Settings)
+	end
+		
+	if KBM.Options.Character then
+		chKBMIROTPEN_Settings = self.Settings
+	else
+		KBMIROTPEN_Settings = self.Settings
+	end	
+end
+
+function EN:SaveVars()	
+	if KBM.Options.Character then
+		chKBMIROTPEN_Settings = self.Settings
+	else
+		KBMIROTPEN_Settings = self.Settings
+	end	
+end
+
+function EN:Castbar(units)
+end
+
+function EN:RemoveUnits(UnitID)
+	if self.Ereandorn.UnitID == UnitID then
+		self.Ereandorn.Available = false
+		return true
+	end
+	return false
+end
+
+function EN:Death(UnitID)
+	if self.Ereandorn.UnitID == UnitID then
+		self.Ereandorn.Dead = true
+		return true
+	end
+	return false
+end
+
+function EN:UnitHPCheck(uDetails, unitID)	
+	if uDetails and unitID then
+		if not uDetails.player then
+			if uDetails.name == self.Ereandorn.Name then
+				if not self.Ereandorn.UnitID then
+					self.EncounterRunning = true
+					self.StartTime = Inspect.Time.Real()
+					self.HeldTime = self.StartTime
+					self.TimeElapsed = 0
+					self.Ereandorn.Casting = false
+					self.Ereandorn.CastBar:Create(unitID)
+					self.PhaseObj:Start(self.StartTime)
+					self.PhaseObj:SetPhase(KBM.Language.Options.Single[KBM.Lang])
+					self.PhaseObj.Objectives:AddPercent(self.Ereandorn.Name, 0, 100)
+					self.Phase = 1
+				end
+				self.Ereandorn.UnitID = unitID
+				self.Ereandorn.Available = true
+				return self.Ereandorn
+			end
+		end
+	end
+end
+
+function EN:Reset()
+	self.EncounterRunning = false
+	self.Ereandorn.Available = false
+	self.Ereandorn.UnitID = nil
+	self.Ereandorn.CastBar:Remove()
+	self.Ereandorn.Dead = false
+	self.PhaseObj:End(Inspect.Time.Real())
+end
+
+function EN:Timer()	
+end
+
+function EN.Ereandorn:SetTimers(bool)	
+	if bool then
+		for TimerID, TimerObj in pairs(self.TimersRef) do
+			TimerObj.Enabled = TimerObj.Settings.Enabled
+		end
+	else
+		for TimerID, TimerObj in pairs(self.TimersRef) do
+			TimerObj.Enabled = false
+		end
+	end
+end
+
+function EN.Ereandorn:SetAlerts(bool)
+	if bool then
+		for AlertID, AlertObj in pairs(self.AlertsRef) do
+			AlertObj.Enabled = AlertObj.Settings.Enabled
+		end
+	else
+		for AlertID, AlertObj in pairs(self.AlertsRef) do
+			AlertObj.Enabled = false
+		end
+	end
+end
+
+function EN:DefineMenu()
+	self.Menu = ROTP.Menu:CreateEncounter(self.Ereandorn, self.Enabled)
+end
+
+function EN:Start()
+	-- Alerts
+	self.Ereandorn.AlertsRef.Combustion = KBM.Alert:Create(self.Lang.Ability.Combustion[KBM.Lang], nil, true, false, "red")
+	self.Ereandorn.AlertsRef.Growth = KBM.Alert:Create(self.Lang.Ability.Growth[KBM.Lang], 8, true, true, "red")
+	self.Ereandorn.AlertsRef.Eruption = KBM.Alert:Create(self.Lang.Ability.Eruption[KBM.Lang], 5, true, false, "orange")
+	KBM.Defaults.AlertObj.Assign(self.Ereandorn)
+	
+	-- Create Mechanic Spies
+	self.Ereandorn.MechRef.Combustion = KBM.MechSpy:Add(self.Lang.Ability.Combustion[KBM.Lang], nil, "playerDebuff", self.Ereandorn)
+	KBM.Defaults.MechObj.Assign(self.Ereandorn)
+		
+	-- Assign mechanics to Triggers
+	self.Ereandorn.Triggers.Combustion = KBM.Trigger:Create(self.Lang.Ability.Combustion[KBM.Lang], "playerDebuff", self.Ereandorn)
+	self.Ereandorn.Triggers.Combustion:AddAlert(self.Ereandorn.AlertsRef.Combustion, true)
+	self.Ereandorn.Triggers.Combustion:AddSpy(self.Ereandorn.MechRef.Combustion)
+	self.Ereandorn.Triggers.Growth = KBM.Trigger:Create(self.Lang.Notify.Growth[KBM.Lang], "notify", self.Ereandorn)
+	self.Ereandorn.Triggers.Growth:AddAlert(self.Ereandorn.AlertsRef.Growth)
+	self.Ereandorn.Triggers.Eruption = KBM.Trigger:Create(self.Lang.Notify.Eruption[KBM.Lang], "notify", self.Ereandorn)
+	self.Ereandorn.Triggers.Eruption:AddAlert(self.Ereandorn.AlertsRef.Eruption)
+	
+	self.Ereandorn.CastBar = KBM.Castbar:Add(self, self.Ereandorn)
+	self.PhaseObj = KBM.PhaseMonitor.Phase:Create(1)
+	
+end
---
+++ Slivers/Prophecy of Ahnket/Rise_of_the_Phoenix/Silgen.lua
@@ -0,0 +1,251 @@
+﻿-- General Silgen Boss Mod for King Boss Mods
+-- Written by Paul Snart
+-- Copyright 2011
+--
+
+KBMIROTPGS_Settings = nil
+-- Link Mods
+local AddonData = Inspect.Addon.Detail("KingMolinator")
+local KBM = AddonData.data
+if not KBM.BossMod then
+	return
+end
+local ROTP = KBM.BossMod["Intrepid Rise of the Phoenix"]
+
+local GS = {
+	Directory = ROTP.Directory,
+	File = "Silgen.lua",
+	Enabled = true,
+	Instance = ROTP.Name,
+	InstanceObj = ROTP,
+	HasPhases = true,
+	Lang = {},
+	ID = "Intrepid Silgen",
+	Enrage = 60 * 7,
+	Object = "GS",
+}
+
+GS.Silgen = {
+	Mod = GS,
+	Level = "72",
+	Active = false,
+	Name = "General Silgen",
+	NameShort = "Silgen",
+	Menu = {},
+	Castbar = nil,
+	Dead = false,
+	TimersRef = {},
+	AlertsRef = {},
+	MechRef = {},
+	Available = false,
+	UTID = "U2B74AD9C721954FE",
+	UnitID = nil,
+	TimeOut = 5,
+	Triggers = {},
+	Settings = {
+		CastBar = KBM.Defaults.Castbar(),
+		TimersRef = {
+			Enabled = true,
+			Funnel = KBM.Defaults.TimerObj.Create("red"),
+			Incinerate = KBM.Defaults.TimerObj.Create("yellow"),
+		},
+		AlertsRef = {
+			Enabled = true,
+			Funnel = KBM.Defaults.AlertObj.Create("red"),
+			Anchor = KBM.Defaults.AlertObj.Create("blue"),
+			Incinerate = KBM.Defaults.AlertObj.Create("yellow"),
+		},
+		MechRef = {
+			Enabled = true,
+			Anchor = KBM.Defaults.MechObj.Create("orange"),
+		},
+	}
+}
+
+KBM.RegisterMod(GS.ID, GS)
+
+-- Main Unit Dictionary
+GS.Lang.Unit = {}
+GS.Lang.Unit.Silgen = KBM.Language:Add(GS.Silgen.Name)
+GS.Lang.Unit.Silgen:SetGerman("General Silgen")
+GS.Lang.Unit.Silgen:SetFrench("Général Silgen")
+GS.Lang.Unit.Silgen:SetRussian("Генерал Силген")
+GS.Lang.Unit.Silgen:SetKorean("실젠 장군")
+GS.Silgen.Name = GS.Lang.Unit.Silgen[KBM.Lang]
+GS.Descript = GS.Silgen.Name
+
+-- Ability Dictionary
+GS.Lang.Ability = {}
+GS.Lang.Ability.Funnel = KBM.Language:Add("Heat Funnel")
+GS.Lang.Ability.Funnel:SetGerman("Hitzetrichter")
+GS.Lang.Ability.Funnel:SetRussian("Раскаленный горн")
+GS.Lang.Ability.Funnel:SetFrench("Conduit de chaleur")
+GS.Lang.Ability.Funnel:SetKorean("열기 깔때기")
+GS.Lang.Ability.Incinerate = KBM.Language:Add("Incinerate")
+GS.Lang.Ability.Incinerate:SetGerman("Verbrennen")
+GS.Lang.Ability.Incinerate:SetRussian("Кремация")
+GS.Lang.Ability.Incinerate:SetFrench("Incinération")
+GS.Lang.Ability.Incinerate:SetKorean("소각")
+
+-- Debuff Dictionary
+GS.Lang.Debuff = {}
+GS.Lang.Debuff.Anchor = KBM.Language:Add("Anchored in Flames")
+GS.Lang.Debuff.Anchor:SetGerman("In Flammen verankert")
+GS.Lang.Debuff.Anchor:SetRussian("Защита огня")
+GS.Lang.Debuff.Anchor:SetFrench("Ancrage de flammes")
+GS.Lang.Debuff.Anchor:SetKorean("불꽃 고정")
+
+function GS:AddBosses(KBM_Boss)
+	self.MenuName = self.Descript
+	self.Bosses = {
+		[self.Silgen.Name] = self.Silgen,
+	}
+end
+
+function GS:InitVars()
+	self.Settings = {
+		Enabled = true,
+		CastBar = self.Silgen.Settings.CastBar,
+		EncTimer = KBM.Defaults.EncTimer(),
+		PhaseMon = KBM.Defaults.PhaseMon(),
+		MechSpy = KBM.Defaults.MechSpy(),
+		MechTimer = KBM.Defaults.MechTimer(),
+		Alerts = KBM.Defaults.Alerts(),
+		TimersRef = self.Silgen.Settings.TimersRef,
+		AlertsRef = self.Silgen.Settings.AlertsRef,
+		MechRef = self.Silgen.Settings.MechRef,
+	}
+	KBMIROTPGS_Settings = self.Settings
+	chKBMIROTPGS_Settings = self.Settings
+	
+end
+
+function GS:SwapSettings(bool)
+
+	if bool then
+		KBMIROTPGS_Settings = self.Settings
+		self.Settings = chKBMIROTPGS_Settings
+	else
+		chKBMIROTPGS_Settings = self.Settings
+		self.Settings = KBMIROTPGS_Settings
+	end
+
+end
+
+function GS:LoadVars()	
+	if KBM.Options.Character then
+		KBM.LoadTable(chKBMIROTPGS_Settings, self.Settings)
+	else
+		KBM.LoadTable(KBMIROTPGS_Settings, self.Settings)
+	end
+	
+	if KBM.Options.Character then
+		chKBMIROTPGS_Settings = self.Settings
+	else
+		KBMIROTPGS_Settings = self.Settings
+	end	
+end
+
+function GS:SaveVars()	
+	if KBM.Options.Character then
+		chKBMIROTPGS_Settings = self.Settings
+	else
+		KBMIROTPGS_Settings = self.Settings
+	end	
+end
+
+function GS:Castbar(units)
+end
+
+function GS:RemoveUnits(UnitID)
+	if self.Silgen.UnitID == UnitID then
+		self.Silgen.Available = false
+		return true
+	end
+	return false
+end
+
+function GS:Death(UnitID)
+	if self.Silgen.UnitID == UnitID then
+		self.Silgen.Dead = true
+		return true
+	end
+	return false
+end
+
+function GS:UnitHPCheck(uDetails, unitID)	
+	if uDetails and unitID then
+		if not uDetails.player then
+			if uDetails.name == self.Silgen.Name then
+				if not self.EncounterRunning then
+					self.EncounterRunning = true
+					self.StartTime = Inspect.Time.Real()
+					self.HeldTime = self.StartTime
+					self.TimeElapsed = 0
+					self.Silgen.Dead = false
+					self.Silgen.Casting = false
+					self.Silgen.CastBar:Create(unitID)
+					self.PhaseObj:Start(self.StartTime)
+					self.PhaseObj:SetPhase(KBM.Language.Options.Single[KBM.Lang])
+					self.PhaseObj.Objectives:AddPercent(self.Silgen.Name, 0, 100)
+					self.Phase = 1
+					KBM.MechTimer:AddStart(self.Silgen.TimersRef.Incinerate)
+				end
+				self.Silgen.UnitID = unitID
+				self.Silgen.Available = true
+				return self.Silgen
+			end
+		end
+	end
+end
+
+function GS:Reset()
+	self.EncounterRunning = false
+	self.Silgen.Available = false
+	self.Silgen.UnitID = nil
+	self.Silgen.CastBar:Remove()
+	self.PhaseObj:End(Inspect.Time.Real())
+end
+
+function GS:Timer()	
+end
+
+function GS:DefineMenu()
+	self.Menu = ROTP.Menu:CreateEncounter(self.Silgen, self.Enabled)
+end
+
+function GS:Start()
+	-- Create Timers
+	self.Silgen.TimersRef.Funnel = KBM.MechTimer:Add(self.Lang.Ability.Funnel[KBM.Lang], 15)
+	self.Silgen.TimersRef.Incinerate = KBM.MechTimer:Add(self.Lang.Ability.Incinerate[KBM.Lang], 60)
+	KBM.Defaults.TimerObj.Assign(self.Silgen)
+	
+	-- Create Alerts
+	self.Silgen.AlertsRef.Funnel = KBM.Alert:Create(self.Lang.Ability.Funnel[KBM.Lang], nil, true, true, "red")
+	self.Silgen.AlertsRef.Anchor = KBM.Alert:Create(self.Lang.Debuff.Anchor[KBM.Lang], nil, true, true, "orange")
+	self.Silgen.AlertsRef.Anchor:Important()
+	self.Silgen.AlertsRef.Incinerate = KBM.Alert:Create(self.Lang.Ability.Incinerate[KBM.Lang], nil, true, true, "yellow")
+	KBM.Defaults.AlertObj.Assign(self.Silgen)
+	
+	-- Create Mechanic Spies
+	self.Silgen.MechRef.Anchor = KBM.MechSpy:Add(self.Lang.Debuff.Anchor[KBM.Lang], nil, "playerDebuff", self.Silgen)
+	KBM.Defaults.MechObj.Assign(self.Silgen)
+	
+	-- Assign Alerts and Timers to Triggers
+	self.Silgen.Triggers.Funnel = KBM.Trigger:Create(self.Lang.Ability.Funnel[KBM.Lang], "channel", self.Silgen)
+	self.Silgen.Triggers.Funnel:AddTimer(self.Silgen.TimersRef.Funnel)
+	self.Silgen.Triggers.Funnel:AddAlert(self.Silgen.AlertsRef.Funnel)
+	self.Silgen.Triggers.Anchor = KBM.Trigger:Create(self.Lang.Debuff.Anchor[KBM.Lang], "playerBuff", self.Silgen)
+	self.Silgen.Triggers.Anchor:AddAlert(self.Silgen.AlertsRef.Anchor, true)
+	self.Silgen.Triggers.Anchor:AddSpy(self.Silgen.MechRef.Anchor)
+	self.Silgen.Triggers.AnchorRemove = KBM.Trigger:Create(self.Lang.Debuff.Anchor[KBM.Lang], "playerBuffRemove", self.Silgen)
+	self.Silgen.Triggers.AnchorRemove:AddStop(self.Silgen.AlertsRef.Anchor)
+	self.Silgen.Triggers.AnchorRemove:AddStop(self.Silgen.MechRef.Anchor)
+	self.Silgen.Triggers.Incinerate = KBM.Trigger:Create(self.Lang.Ability.Incinerate[KBM.Lang], "channel", self.Silgen)
+	self.Silgen.Triggers.Incinerate:AddTimer(self.Silgen.TimersRef.Incinerate)
+	self.Silgen.Triggers.Incinerate:AddAlert(self.Silgen.AlertsRef.Incinerate)
+	
+	self.Silgen.CastBar = KBM.Castbar:Add(self, self.Silgen)
+	self.PhaseObj = KBM.PhaseMonitor.Phase:Create(1)
+	
+end
